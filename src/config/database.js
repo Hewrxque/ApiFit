76876456158be010f849cec9167a438ce2d19900
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-05-05 13:36:17 -0300

    update

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+dialect: 'mysql',
+host: 'localhost',
+username: 'root',
+password: 'root',
+database: 'Api_Fit',
+define: {
+    timestamps: true,
+    undescored: true,
+},
+};
+
