896182c186136bae566cc55bc8d1b605d7a4a3a3
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-05-07 21:12:54 -0300

    update

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,11 +1,17 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 module.exports = {
-    async store(req, res){
-            const {name, email, password } = req.body;
+  async index(req, res) {
+    const users = await User.findAll();
 
-            const user = await User.create({ name, email, password});
+    return res.json(users);
+  },
 
-            return res.json(user);
-    }
-}
+  async store(req, res) {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({ name, email, password });
+
+    return res.json(user);
+  },
+};
